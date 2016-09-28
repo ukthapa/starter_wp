0a821c7690989e1824d857cb6c383f0749872dbd
System Administrator <root@Uttams-iMac.local>
2016-09-28 18:36:16 +0530

    Workflow configuration done

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+// Project specific variables
+var url = 'http://blog.localhost/'; // Local dev URL. Change as needed.
+
+// Load plugins
+var gulp = require('gulp'),
+	stylus = require('gulp-stylus'),
+	jeet = require('jeet'), // stylus grid
+	autoprefixer = require('gulp-autoprefixer'),
+	minifyCss = require('gulp-minify-css'),
+	rename = require('gulp-rename'),
+	plumber = require('gulp-plumber'),
+	notify = require('gulp-notify'),
+	browserSync = require('browser-sync'),
+	reload = browserSync.reload;
+
+// Styles
+gulp.task('styles', function(){
+	 gulp.src('./styl/*.styl') // Two files get compiled here: main stylsheet (all partials imported) and editor stylesheet. Makes for simple gulpfile config, but maybe not best approach. Comments welcome! 
+	 .pipe(plumber({
+	    errorHandler: notify.onError("Error: <%= error.message %>")
+	  }))
+ 	.pipe(stylus({
+       use: [jeet()],
+       sourcemap: { inline: true }
+     }))
+	.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+	.pipe(gulp.dest('./'))
+	.pipe(reload({ stream : true }))
+	// ---------------------------------------
+	// Uncomment next 3 lines for minified css
+	// ---------------------------------------
+	// .pipe(rename({ suffix: '.min' }))
+	// .pipe(minifyCss())
+	// .pipe(gulp.dest('./'))
+});
+
+// Browser Sync
+gulp.task('browser-sync', function(){
+	browserSync({
+		proxy: url
+		})
+});
+
+// Watch!
+gulp.task('watch', ['browser-sync'], function(){
+	gulp.watch('./styl/**/*.styl', ['styles']);
+});
